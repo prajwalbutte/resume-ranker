45bc91a3703ae5fa2a7dbf52db9576c8159ea380
prajwalbutte <prajwaldbutte@gmail.com>
2025-06-26 12:49:06 +0530

    Add files via upload

---
+++ project_code.py
@@ -0,0 +1,32 @@
+print("Resume Ranker - Applicant Tracking System Python Project\n\n")
+
+#Input
+job_description = input("Paste Job Description:\n").lower()
+resume = input("Paste Resume Here:\n").lower()
+
+
+#Processing Input
+jd_words = job_description.split()
+resume_words = resume.split()
+
+#Finding matching words
+match = 0
+for word in jd_words:
+    if word in resume_words:
+        match += 1 
+
+#Score Calculating
+score = int((match/len(set(jd_words)))*100)
+
+#Outputs
+print(f"\nYour resume match score is:{score}%")
+
+if score >= 75 :
+    print("Strong match,Your resume files the job well.")
+elif score >= 40 :
+    print("Average match,Try improving your resume.")
+else:
+    print("Weak match..!Customize your resume better.")
+
+
+print("Thank You..!\n")
